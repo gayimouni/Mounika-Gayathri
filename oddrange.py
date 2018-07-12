1ece3d0930755fe5820f23fed832d7221502d445
gayimouni <33306876+gayimouni@users.noreply.github.com>
2018-07-12 11:52:51 +0530

    Create oddrange.py

---
+++ oddrange.py
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+int x,byi;
+
+printf("enter the two limits");
+
+scanf("%d%d",&x,&y);
+
+for(i=x;i<y;i++)
+
+{
+
+if(i%2==1)
+
+{
+
+printf("%d",i);
+
+}
+
+}
+}
