0e110d0a4ab988a723653c2f181528291ce9f55f
gayimouni <33306876+gayimouni@users.noreply.github.com>
2018-07-13 12:12:21 +0530

    Add files via upload

---
+++ arms.py
@@ -0,0 +1,18 @@
+def main():
+   
+ num = int(input("Enter a number: "))
+ sum = 0
+ temp = num
+ while temp > 0:
+   dig = temp % 10
+   sum += dig ** 3
+   temp //= 10
+ if num == sum:
+   print("Yes")
+ else:
+   print("No")
+
+if __name__ == '__main__':
+    main()
+
+   
