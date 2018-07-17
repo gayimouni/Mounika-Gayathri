db9f0d83ded99996fd9efdd450b0230514cff563
gayimouni <33306876+gayimouni@users.noreply.github.com>
2018-07-17 11:54:00 +0530

    Update fa.py

--- fa.py
+++ fa.py
@@ -1,9 +1,5 @@
 def factorial(n):
-     
-    # single line to find factorial
-    return 1 if (n==1 or n==0) else n * factorial(n - 1); 
- 
-# Driver Code
+     return 1 if (n==1 or n==0) else n * factorial(n - 1); 
 num=5
 print("Factorial of",num,"is",
 factorial(num))
